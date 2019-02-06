3339870169a9e95e2f4432e870c8c8ab1cd6fa6b
paflov <philipp.franck@dainst.de>
2019-02-06 12:46:10 +0100

    a script to test latex export and rendering at once

---
+++ renderExport.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+curl -X GET \
+  'http://localhost:8080/data/catalog/782?mediaType=tex' \
+  -H 'Authorization: Basic cC5mcmFuY2s6MzliNGI3N2EwYzgxZGU2YTAwYTkwNzdlN2UyMzg0MTY=' \
+  -H 'Cache-Control: no-cache' \
+  -o files/export.tex
+docker-compose up
