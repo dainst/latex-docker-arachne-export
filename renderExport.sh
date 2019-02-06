1fdfc56881b420bbe59a426e83a209f0ade4329f
paflov <philipp.franck@dainst.de>
2019-02-06 12:46:36 +0100

    clean files dir before starting

--- renderExport.sh
+++ renderExport.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+rm files/*
 curl -X GET \
   'http://localhost:8080/data/catalog/782?mediaType=tex' \
   -H 'Authorization: Basic cC5mcmFuY2s6MzliNGI3N2EwYzgxZGU2YTAwYTkwNzdlN2UyMzg0MTY=' \
