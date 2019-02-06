03f97a8ef921576cc58f660ecb19ba8c872558f4
paflov <philipp.franck@dainst.de>
2019-02-06 12:45:08 +0100

    fixed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ cd /latex-dainst-bundle/dainst-bundle
 for file in /files/*.tex
 do
     filename=`basename -s .tex ${file}`
-    echo "try to render ${filename}"
+    echo "Try to render ${filename}"
     make FILE=${filename} render
 done
